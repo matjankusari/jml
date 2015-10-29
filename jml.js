9484a07119a29e00c66e4c65e357b3dd2743cbb6
matjankusari <matjankusari@gmail.com>
2015-10-29 20:13:13 +0100

    initial versio of jml

---
+++ jml.js
@@ -0,0 +1,85 @@
+//var mat = <title>The HTML5 Herald</title>;
+
+var jml = {
+
+    body: {
+        'div class=mat uuid123': {
+            span: {
+                t: "No, np, hello u"
+            },
+            table: {
+                h2: {
+                    t: "more text"
+                },
+                'a href="http://www.google.com"' : {
+                    t:'text'
+                }
+            }
+        },
+        'div': {
+            span: {
+                button: {
+                    t: "sdfsdfdffsd"
+                }
+            }
+        },
+        myeleemnt: {
+            anopther: {
+                yeanother: {
+                    t:"ddfgdfgsdfgf"
+                }
+            },
+            kkkkk: {
+                t:"sdfsdf"
+            }
+        },
+        'mat class="mat"': {
+            t: "sdfsdf"
+        },
+        blink: someHtml()
+    }
+}
+
+
+function someHtml() {
+    return {
+        div: {
+            span: {
+                t: "sdffsdf"
+            }
+        }
+    }
+}
+
+
+function getHtml( jml ) {
+ 
+    var html = [];
+
+    Object.keys( jml ).forEach( function( key ) {
+
+        if ( typeof jml[key] === "string") 
+        {   
+            html.push( wrap( key, jml[key]) );
+        }
+        else if( typeof jml[key] === "object") 
+        {
+            html.push( wrap( key, getHtml( jml[key] )));        
+        }  
+        else 
+        {
+            throw new Error("invalid node type: " + key );
+        }
+    });
+
+    return html.join("");
+}
+
+function wrap( tag, html ) 
+{
+    return tag === "t" ? html : '<'+tag+'>'+html+'</'+tag.split(" ")[0]+'>';
+}
+
+var html = getHtml( jml ); 
+
+console.log(html);
